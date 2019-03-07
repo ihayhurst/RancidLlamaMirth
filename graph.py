b4a655c31a2ea062ad1b97c3b16c6ae6881413b7
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-03-07 21:30:46 +0000

    Updated graph.py, changed logfile format, added more configs, corrected spellings, updated gitignore and allowed using configs for the graph script

--- graph.py
+++ graph.py
@@ -1,22 +1,33 @@
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import re
-x=[0,5,10,15,20,25,30,35,40,45,50,55]
+x=[0]
 y=[]
 
-def generateGraph():
+def generateGraph(reading_count_priv, time_interval_priv):
+  global reading_count
+  global time_interval
+  reading_count = reading_count_priv
+  time_interval = time_interval_priv
+  for count in range(1, reading_count_priv):
+    x.append(count * time_interval_priv)
+  produceGraph()
+
+def produceGraph():
 
   with open("temps.log", "r") as f:
-      taildata = f.readlines() [-12:]
+      taildata = f.readlines() [-reading_count:]
 
   for line in taildata:
-      data = re.findall(r"[\w']+", line)
-      #x.append(int(data[0]))
+      #data = re.findall(r"[\w']+", line)
+      data = re.findall(r"[\w:][0-5][0-9]+", line)
+      print(data)
       y.append(int(data[1]))
 
-  plt.plot(x,y, label='Temperature  °C')
-  plt.xlabel('Time')
-  plt.ylabel('Temperature')
-  plt.title(' Latest 12 readings\nHot off the Pi')
+  plt.plot(x,y, label='Temperature °C')
+  plt.xlabel('Time (Last x number of mins)')
+  plt.ylabel('Temperature (°C)')
+  plt.title(' Last ' + str(reading_count) + ' readings\nHot off the Pi')
   plt.legend()
-  #plt.show()
   plt.savefig("graph.png")
