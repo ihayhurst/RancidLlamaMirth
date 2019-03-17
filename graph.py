26c8613f30b9ee3c98f6b7343b226abee1cfd5cd
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-03-17 16:40:01 +0000

    Added improved graphing script

---
+++ graph.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+import re, datetime
+
+def generateGraph(reading_count, font_path):
+  x, y  = readValues(reading_count)
+  if x == '':
+    print('Not enough lines in logfile, aborting\n')
+    produceText(font_path)
+    return
+  drawGraph(x,y)
+
+def produceText(font_path):
+  img = Image.new('RGB', (450, 40), color = (0, 0, 0))
+  fnt = ImageFont.truetype(font_path, 20)
+  d = ImageDraw.Draw(img)
+  d.text((20,10), "Not enough lines in the log to generate a graph", font=fnt, fill=(40, 231, 35))
+  img.save('graph.png')
+
+def drawGraph(x,y):
+    plt.grid(b=None, which='major', axis='both')
+    plt.plot(x,y, label='Temperature in \u2103')
+    plt.gcf().autofmt_xdate() 
+    plt.xlabel('Time (Day - Hour: Minutes)')
+    plt.ylabel("Temperature \u2103")
+    plt.title('Conservatory Temperature logged by Pi')
+    plt.savefig("graph.png")
+    print('Created graph\n')
+    plt.clf()
+
+def readValues(x=[], y=[], reading_count):
+    with open("temps.log", "r") as f:
+        taildata = f.readlines() [-reading_count:]
+        if reading_count > len(taildata):
+          return ['', '']
+        for line in taildata:
+            data = re.split("\[(.*?)\]", line)
+            temp = re.findall("\d+\.\d+", data[2]) 
+            temp = float(temp[0])
+            dt = datetime.datetime.strptime(data[1], "%a %b %d %H:%M:%S %Y")
+            x.append(dt)
+            y.append(temp)
+        return x,y
+
+generateGraph(12, 'fonts/Lato-Regular.ttf')
