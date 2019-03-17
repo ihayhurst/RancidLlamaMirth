bd436a18df6947377868ed221496eaf54f1eb72e
Dragon8oy <stuart.a.hayhurst@gmail.com>
2019-03-17 17:09:27 +0000

    Added support for choosing between scatter and line graphs

--- graph.py
+++ graph.py
@@ -3,15 +3,15 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw, ImageFont
-import re, datetime
+import re, datetime, csv
 
-def generateGraph(reading_count, font_path):
+def generateGraph(reading_count, font_path, chart_type):
   x, y  = readValues(reading_count)
   if x == '':
     print('Not enough lines in logfile, aborting\n')
     produceText(font_path)
     return
-  drawGraph(x,y)
+  drawGraph(x,y,chart_type)
 
 def produceText(font_path):
   img = Image.new('RGB', (450, 40), color = (0, 0, 0))
@@ -20,9 +20,16 @@ def produceText(font_path):
   d.text((20,10), "Not enough lines in the log to generate a graph", font=fnt, fill=(40, 231, 35))
   img.save('graph.png')
 
-def drawGraph(x,y):
-    plt.grid(b=None, which='major', axis='both')
-    plt.plot(x,y, label='Temperature in \u2103')
+def drawGraph(x,y,chart_type):
+    if chart_type == 'line':
+      plt.grid(b=None, which='major', axis='both')
+      plt.plot(x,y, label='Temperature in \u2103')
+    elif chart_type == 'scatter':
+      plt.style.use('ggplot')
+      plt.plot([],[])
+      plt.scatter(x,y, marker=".")
+    else:
+      print('Invalid config option')
     plt.gcf().autofmt_xdate() 
     plt.xlabel('Time (Day - Hour: Minutes)')
     plt.ylabel("Temperature \u2103")
@@ -31,7 +38,7 @@ def drawGraph(x,y):
     print('Created graph\n')
     plt.clf()
 
-def readValues(x=[], y=[], reading_count):
+def readValues(reading_count, x=[], y=[]):
     with open("temps.log", "r") as f:
         taildata = f.readlines() [-reading_count:]
         if reading_count > len(taildata):
@@ -44,5 +51,3 @@ def readValues(x=[], y=[], reading_count):
             x.append(dt)
             y.append(temp)
         return x,y
-
-generateGraph(12, 'fonts/Lato-Regular.ttf')
