f81aad2d8b6f6db65084ccf75ad82f92870b8f07
ihayhurst <29225030+ihayhurst@users.noreply.github.com>
2019-04-14 12:28:43 +0100

    modified:   graph.py             working range or tail modes

--- graph.py
+++ graph.py
@@ -42,32 +42,42 @@ def drawGraph(x,y):
     plt.clf()
 
 def readValues(*args, **kwargs):
-    #for key, value in kwargs.items(): 
-        #print ("%s == %s" %(key, value))
+    for key, value in kwargs.items(): 
+        print ("%s == %s" %(key, value))
     print(reading_count)
-    if kwargs['from_date']: from_date = kwargs.get('from_date')
-    if kwargs['to_date']: to_date = kwargs.get('to_date')
-
+    log_dt_format = '%a %b %d %H:%M:%S %Y'
+    dt_format = '%Y/%m/%d-%H:%M'
     x=[]
     y=[]
     x.clear()
     y.clear()
-    log_dt_format = '%a %b %d %H:%M:%S %Y'
-    dt_format = '%Y/%m/%d-%H:%M'
-    #from_dt, to_dt = '2019/03/30-00:00', '2019/04/05-23:59'
-    print(from_date, to_date)
-    from_dt = datetime.datetime.strptime(from_date, dt_format)
-    to_dt = datetime.datetime.strptime(to_date, dt_format)
+    tailmode= kwargs.get('tailmode')
+    if not tailmode:
+        from_date = kwargs.get('from_date')
+        from_dt = datetime.datetime.strptime(from_date, dt_format)
+        to_date = kwargs.get('to_date')
+        to_dt = datetime.datetime.strptime(to_date, dt_format)
+        from_dt = datetime.datetime.strptime(from_date, dt_format)
+        to_dt = datetime.datetime.strptime(to_date, dt_format)
+
     with open('temps.log', 'r') as f:
-        for line in f:
+        if tailmode:
+            taildata = f.readlines() [-reading_count:]
+        else:
+            taildata = f.readlines()
+        for line in taildata:
             data = re.split("\[(.*?)\]", line)
             if len(data) !=3: continue #ignore lines that don't have 3 elements
             temp = re.findall("\d+\.\d+", data[2]) 
             temp = float(temp[0])
             dt = datetime.datetime.strptime(data[1], log_dt_format)
-            if (dt >= from_dt) and (dt <= to_dt):
+            if tailmode: 
                 x.append(dt)
                 y.append(temp)
+            else:
+                if (dt >= from_dt) and (dt <= to_dt):
+                    x.append(dt)
+                    y.append(temp)
 
         return x,y
 
@@ -76,12 +86,16 @@ if __name__ == '__main__':
         reading_count = int(sys.argv[1])
     except IndexError:
         print('python3 graph.py [int] :Optional integer number of readings to plot\n')
-        print( '                        set at last 12 readings if not specified\n')
-        reading_count = int(12)
+        reading_count = int(0)
     except ValueError:
         print('Needs to be an integer')
         sys.exit(1)
-    kwargs={'from_date' : '2019/03/27-00:00', 'to_date' : '2019/03/30-00:00' }
+
+    if reading_count == 0:
+         kwargs={'tailmode': False, 'from_date' : '2019/03/29-00:00', 'to_date' : '2019/03/30-00:00' }
+    else:
+        kwargs={'tailmode' : True}
+
     args={reading_count}
     x, y  = readValues(*args, **kwargs) 
     drawGraph(x,y)
