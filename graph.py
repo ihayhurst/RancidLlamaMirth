3b548562abea29167a2139b4524f44f8dc6fa5e1
Ian Hayhurst <ihayhurst@.users.noreply.github.com>
2019-05-14 16:53:49 +0100

    modified:   graph.py  Duration call for timdedelta working (fixed at 1W for now)                           parse_duration needed to return number and unit for timedelta

--- graph.py
+++ graph.py
@@ -7,14 +7,14 @@ from scipy.interpolate import make_interp_spline, BSpline
 import numpy as np
 import sys, argparse, re, datetime
 
-DT_FORMAT           = '%Y/%m/%d-%H:%M'
-LOG_DT_FORMAT   = '%a %b %d %H:%M:%S %Y' 
+DT_FORMAT       = '%Y/%m/%d-%H:%M'
+LOG_DT_FORMAT   = '%a %b %d %H:%M:%S %Y'
 
 def generateGraph(reading_count):
     '''Wrapper for drawgraph called from '''
-    kwargs={'tailmode' : True}
-    args = {reading_count}
-    x, y  = readValues(*args, **kwargs)
+    kwargs = {'tailmode' : True}
+    args   = {reading_count}
+    x, y   = readValues(*args, **kwargs)
     if x == '':
       print('Not enough lines in logfile, aborting\n')
       return
@@ -32,9 +32,9 @@ def drawGraph(x,y):
 
     plt.grid(b=True, which='major', axis='both', color='black')
     plt.plot([],[])
-    x_smooth_dt = mdates.num2date(x_smooth) 
+    x_smooth_dt = mdates.num2date(x_smooth)
     plt.plot(x_smooth_dt, y_smooth, 'red', linewidth=1)
-    plt.gcf().autofmt_xdate() 
+    plt.gcf().autofmt_xdate()
     plt.xlabel('Time (Month-Day - Hour: Minutes)')
     plt.ylabel('Temperature \u2103')
     plt.title('Room Temperature logged by Pi')
@@ -45,12 +45,12 @@ def drawGraph(x,y):
 def readValues(*args, **kwargs):
     for key, value in kwargs.items():     #Debug
         print ("%s == %s" %(key, value)) #Debug
-        
+
     if kwargs.get('lines') != None:
         reading_count = kwargs.get('lines')
     else:
         reading_count = args[0]
-                
+
     log_dt_format = '%a %b %d %H:%M:%S %Y'
 
     x=[]
@@ -64,7 +64,7 @@ def readValues(*args, **kwargs):
     if not tailmode:
         from_dt = date_to_dt(kwargs.get('from_date'),DT_FORMAT)
         to_dt = date_to_dt(kwargs.get('to_date'),DT_FORMAT)
-        
+
     with open('temps.log', 'r') as f:
         if tailmode:
             taildata = f.readlines() [-reading_count:]
@@ -73,10 +73,10 @@ def readValues(*args, **kwargs):
         for line in taildata:
             data = re.split("\[(.*?)\]", line)
             if len(data) !=3: continue #ignore lines that don't have 3 elements
-            temp = re.findall("[-]?\d+\.\d+", data[2]) 
+            temp = re.findall("[-]?\d+\.\d+", data[2])
             temp = float(temp[0])
             dt = date_to_dt(data[1], LOG_DT_FORMAT)
-            if tailmode: 
+            if tailmode:
                 x.append(dt)
                 y.append(temp)
             else:
@@ -91,12 +91,12 @@ def cmd_args(args=None):
 
     parser.add_argument('-l', '--lines',  type=int, dest='lines', default=12,
                     help='Number of tailing log lines to plot')
-    parser.add_argument('-s', '--start',  dest='start', 
-                    help='Start date YYYY/MM/DD-HH:MM') 
-    parser.add_argument('-e', '--end',  dest='end', 
-                    help='End   date YYYY/MM/DD-HH:MM') 
-    parser.add_argument('-d', '--dur',  dest='dur', 
-                    help='Duration: Hours, Days, Weeks, Months e.g. 2W for 2 weeks') 
+    parser.add_argument('-s', '--start',  dest='start',
+                    help='Start date YYYY/MM/DD-HH:MM')
+    parser.add_argument('-e', '--end',  dest='end',
+                    help='End   date YYYY/MM/DD-HH:MM')
+    parser.add_argument('-d', '--dur',  dest='dur',
+                    help='Duration: Hours, Days, Weeks, Months e.g. 2W for 2 weeks')
 
     opt = parser.parse_args(args)
 
@@ -104,7 +104,7 @@ def cmd_args(args=None):
 
 def parse_duration(duration):
     print("Duration == ",duration)
-    duration_delta = datetime.datetime.timedelta(days=7)
+    #duration_delta = datetime.datetime.timedelta(days=7)
     return duration_delta.datetime.datetime.strftime("%d/%m/%Y")
 
 def date_to_dt(datestring, FORMAT):
@@ -114,26 +114,29 @@ def date_to_dt(datestring, FORMAT):
 def main(args=None):
     opt = cmd_args(args)
     kwargs = {}
- 
-    if opt.dur and opt.start and opt.end: #Assume Start and range ignore end
-        print("all three madness")
-    if opt.dur and opt.start and not opt.end: #Start and range
-        print("Start & Duration")
-        #opt.end = opt.start+parse_duration(opt.dur)
-        #kwargs={'tailmode': False, 'from_date': opt.start, 'to_date': opt.end, **kwargs}
+
+    if opt.dur and opt.start and opt.end: #Assume Start and range ignore end //TODO//
+        print("all three madness") #Debug
         
-    if opt.dur and not opt.start and opt.end: #Range before enddate 
-        print("End and Duration")
-    if opt.dur and not opt.start and not opt.end: #tailmode with range
-        print("call from end back to duratiion")
+    if opt.dur and opt.start and not opt.end: #Start and range //TODO- parseduration call//
+        print("Start & Duration") #Debug
+        opt.end_dt = date_to_dt(opt.start, DT_FORMAT)+datetime.timedelta(days=7)
+        opt.end = opt.end_dt.strftime(DT_FORMAT)
+        kwargs={'tailmode': False, 'from_date': opt.start, 'to_date': opt.end, **kwargs}
+
+    if opt.dur and not opt.start and opt.end: #Range before enddate //TODO//
+        print("End and Duration") #Debug
         
+    if opt.dur and not opt.start and not opt.end: #tailmode with range //TODO//
+        print("call from end back to duratiion") #Debug
+
     if not opt.dur and opt.start and opt.end: #Date range
         kwargs={'tailmode': False, 'from_date': opt.start, 'to_date': opt.end, **kwargs}
-        
+
     if not opt.dur and not opt.start and not opt.end: #tailmode with lines
         kwargs={'tailmode': True, 'lines': opt.lines, **kwargs}
 
-    x, y  = readValues(*args, **kwargs) 
+    x, y  = readValues(*args, **kwargs)
     drawGraph(x,y)
     sys.exit(1)
 
